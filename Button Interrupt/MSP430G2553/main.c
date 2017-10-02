ba55db33e44f0ca31d749f1318eef12c7baae80d
Joshua <gouldj5@students.rowan.edu>
2017-10-02 16:34:39 -0400

    button interupt G2553

---
+++ Button Interrupt/MSP430G2553/main.c
@@ -0,0 +1,27 @@
+#include <msp430.h>
+
+#define BUTTON BIT3		//P1.3 BUTTON defined
+#define LED   BIT6		//P1.6 LED green defined
+
+void main(void) {
+	WDTCTL = WDTPW | WDTHOLD;	//stop watchdog timer
+
+	P1DIR |= LED;				//set LED pin to output
+	P1DIR &= ~BUTTON;			//set button to input
+	P1REN |= BUTTON;			//Enable pull-resistor
+	P1OUT |= BUTTON;			//Make pull-resistor a pull-up
+
+	P1IES &= ~BUTTON;			//Rising edge interrupt
+	P1IE |= BUTTON;		 		//Enable interrupt on BUTTON
+
+	_bis_SR_register(LPM4_bits + GIE);
+								//Enter LPM4 and Enable CPU Interrupt
+								// enables interupts
+}
+
+#pragma vector=PORT1_VECTOR		//figure out what this does
+_interrupt void Port_1(void)
+{
+	P1OUT ^= LED;				//Toggle green LED
+	P1IFG &= ~BUTTON;			//Clear BUTTON interrupt flag
+}
